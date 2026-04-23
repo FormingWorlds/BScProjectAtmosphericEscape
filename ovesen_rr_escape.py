259f395456d35c3fa0623359dc770405dea5b9d6
ma1inda <malinleirv@gmail.com>
2026-04-23 11:01:25 +0200

    added calculation for sound speed in escape func

--- ovesen_rr_escape.py
+++ ovesen_rr_escape.py
@@ -12,10 +12,16 @@ def rr_escape_rate():
     Calculates the radiation-recombination-limited escape rate.
 
     Takes input parameters: x [unit], y [unit], z [unit], ...
+
+    All calculations done in SI units.
     '''
 
     #M_rr_rate = -4 * pi* rho_s * c_s * R_s**2   REFERENCE: Lopez 2017 eq. 4
 
+    ### calculates c_s based on input ###
+    k_b = sp.constants.k 
+    m_p = sp.constants.m_p
+    c_s = np.sqrt((k_b * T_wind) / (mu_wind * m_p)) #sound speed, where k_B: Boltzmann constant, T: temperature, mu: mean molecular weight, m_p: proton mass
     
-    
-    return none # Placeholder for the actual implementation
+    return None # Placeholder for the actual implementation
+
