947f64e928abad5726154443afcc18777d63cf05
ma1inda <malinleirv@gmail.com>
2026-04-23 10:52:33 +0200

    Defined skeleton escape function

--- ovesen_rr_escape.py
+++ ovesen_rr_escape.py
@@ -6,3 +6,16 @@ Radition-recombination-limited escape mechanism.
 
 import numpy as np
 import scipy as sp
+
+def rr_escape_rate():
+    '''
+    Calculates the radiation-recombination-limited escape rate.
+
+    Takes input parameters: x [unit], y [unit], z [unit], ...
+    '''
+
+    #M_rr_rate = -4 * pi* rho_s * c_s * R_s**2   REFERENCE: Lopez 2017 eq. 4
+
+    
+    
+    return none # Placeholder for the actual implementation
