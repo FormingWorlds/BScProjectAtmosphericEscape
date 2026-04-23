cda94e1d620b05eaeebd8fa89c64218b1bef8d70
ma1inda <malinleirv@gmail.com>
2026-04-23 13:03:55 +0200

    esc: added placeholder func for R_base finding. fixed typo. esc func: added return of M_dot

--- ovesen_rr_escape.py
+++ ovesen_rr_escape.py
@@ -7,8 +7,14 @@ Radiation-recombination-limited escape mechanism.
 import numpy as np
 import scipy as sp
 
+def find_R_base():
+    #placeholder for finding R_base based on input parameters
+    R_base = None
+    return R_base
+
+
 #In the input box I put the so-far necessary inputs from atmosphere profile for this func to work
-def rr_escape_rate(T_wind, mu_wind, M_p, F_xuv, sigma_nu0, nu_0, alpha_rec_B, mu_plus_wind, R_base):
+def rr_escape_rate(T_wind, mu_wind, M_p, F_xuv, nu_0, alpha_rec_B, mu_plus_wind, R_base):
     '''
     Calculates the radiation-recombination-limited escape rate.
 
@@ -32,6 +38,9 @@ def rr_escape_rate(T_wind, mu_wind, M_p, F_xuv, sigma_nu0, nu_0, alpha_rec_B, mu
     #!!!need to remember to check if R_s > R_base, where R_base is the radius of the base of the escaping atmosphere, otherwise the escape rate is not radiation-recombination-limited (?). I can set R_s = R_base if smaller
     
 
+    ### finds R_base based on input ###
+    #placeholder
+
     ### calculates rho_s based on input ### 
     # reference formula: rho_s = rho_base * exp((-G * M_p) / (R_base * c_s**2) * (R_base/R_s - 1 )) 
 
@@ -42,5 +51,7 @@ def rr_escape_rate(T_wind, mu_wind, M_p, F_xuv, sigma_nu0, nu_0, alpha_rec_B, mu
     rho_base = n_plus_base * mu_plus_wind * m_p #density at the base of the escaping atmosphere, where n_plus_base: number density of the escaping atmosphere at the base, mu_wind: mean molecular weight, m_p: proton mass
     rho_s = rho_base * np.exp((-G * M_p) / (R_base * c_s**2) * (R_base/R_s - 1 )) #rho_s is the density at the sonic point, rho_base: density at the base of the escaping atmosphere, R_base: radius of the base of the escaping atmosphere
 
-    return None # Placeholder for the actual implementation
+    M_rr_rate = -4 * np.pi* rho_s * c_s * R_s**2
+
+    return M_rr_rate 
 
