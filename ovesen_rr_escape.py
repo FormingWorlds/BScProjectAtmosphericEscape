a18c2bac4dbf47864d28096fa583a52d3311dc7f
ma1inda <malinleirv@gmail.com>
2026-04-23 11:09:24 +0200

    esc func: added calculation for radius at sonic point, started tracking necessary input variables

--- ovesen_rr_escape.py
+++ ovesen_rr_escape.py
@@ -7,7 +7,8 @@ Radition-recombination-limited escape mechanism.
 import numpy as np
 import scipy as sp
 
-def rr_escape_rate():
+#In the input box I put the so-far necessary inputs from atmosphere profile for this func to work
+def rr_escape_rate(T_wind, mu_wind, M_p):
     '''
     Calculates the radiation-recombination-limited escape rate.
 
@@ -22,6 +23,10 @@ def rr_escape_rate():
     k_b = sp.constants.k 
     m_p = sp.constants.m_p
     c_s = np.sqrt((k_b * T_wind) / (mu_wind * m_p)) #sound speed, where k_B: Boltzmann constant, T: temperature, mu: mean molecular weight, m_p: proton mass
+    
+    ### calculates rho_s based on input ###
+    G = sp.constants.G
+    R_s = G * M_p / (2 * c_s**2) #radius to the sonic point, where G: gravitational constant, M_p: planetary mass, c_s: sound speed
     
     return None # Placeholder for the actual implementation
 
