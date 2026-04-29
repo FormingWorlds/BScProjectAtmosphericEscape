623bfca98bdd0b81e75096e8c8203b514a89a26a
ioana07x <balintioanaroxana@gmail.com>
2026-04-29 09:55:31 +0200

    Added exobase function

---
+++ exobase.py
@@ -0,0 +1,12 @@
+from scipy.constants import G, k
+# walking up to exobase
+def find_exobase(r, n_tot, T, m_mean, M, sigma):
+    g = G * M / r**2
+    H = k * T / (m_mean * g)
+    mfp = 1 / (sigma * n_tot)
+    
+    idx = np.where(mfp > H)[0]
+    if len(idx) == 0:
+        raise ValueError("No exobase found")
+    
+    return idx[0]
