53c5ac93e72718a2cd4d2b1923376461c79ce6a0
ma1inda <malinleirv@gmail.com>
2026-04-30 10:44:38 +0200

    added H2He mix atmosphere with subneptune-like planet

---
+++ atmospheres/simple_H2He_mix.py
@@ -0,0 +1,28 @@
+import scipy as sp
+import numpy as np
+
+G = sp.constants.G #[m^3 kg^-1 s^-2]
+m_p = sp.constants.m_p #[kg]
+k_b = sp.constants.k #[J K^-1]
+
+#A properties of a simple isothermal, single species (H_2) atmosphere
+
+#Quantity    Value                          Unit         Description
+T_wind       = 10**4                        #[K]         temperature of the escaping atmosphere
+mu_wind      = 0.62                          #[dimless]   total mean molecular weight of the escaping atmosphere assumed to be entirely ionised
+mu_plus_wind = 1.3                          #[dimless]   mean molecular weight of the ions in the escaping atmosphere assumed to be entirely ionised
+
+#testcase values for the other necessary input parameters for the rr_escape_rate function using jupiter mass and radius, and a typical XUV flux for a close-in exoplanet
+M_p          = 5.9722 * 10**24               #[kg]        planetary mass
+#R_base     = 7.1492 * 10**7                #[m]         radius of the base of the escaping atmosphere
+F_xuv        = 1.361 * 10**5                #[kg s^-3]    XUV flux. 100 times the solar flux of earth
+nu_0         = 4.835981008048 * 10**15      #[Hz]        20eV. Maybe this value because of helium in mix. For ionising the atoms.
+R_p          = 2 * 6.371 * 10**6            #[m]         planetary radius. 2 times earth radius
+P_0          = 2000                         #[Pa]        pressure at the optical photosphere
+T_eq         = 1500                         #[K]         equilibrium temperature of the planet
+mu_photo     = 2.5                            #[dimless]   mean molecular weight at the optical photosphere, assumed to be entirely molecular hydrogen
+
+radii = np.linspace(R_p, 5*R_p, 1000) #[m] array of radii from the planetary radius to 10 times the planetary radius
+
+pressures = P_0 * np.exp(G * M_p * mu_photo * m_p / (k_b * T_eq) * (1/radii - 1/R_p) ) #[Pa] pressure profile of the atmosphere based on the barometric formula, where P_0: pressure at the optical photosphere, G: gravitational constant, M_p: planetary mass, mu_photo: mean molecular weight at the optical photosphere, m_p: proton mass, k_b: Boltzmann constant, T_eq: equilibrium temperature of the planet, radii: array of radii from the planetary radius to 10 times the planetary radius
+
