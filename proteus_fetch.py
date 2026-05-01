58813fb8aaef801c04a8c49614185fb45e31badc
ma1inda <malinleirv@gmail.com>
2026-05-01 15:17:43 +0200

    Put case 1 H2 atmosphere into atm object

--- proteus_fetch.py
+++ proteus_fetch.py
@@ -1,5 +1,6 @@
 ### Importing Proteus atmospheres that we want to examine ###
 import pandas as pd
+from atmospheres.atmosphere_setting import Atmosphere
 
 df_planet_properties = pd.read_csv("atmospheres/ProteusGoogleDrive/planet_bulk_properties_H2_atmospheres.csv", sep='\t')
 #print(df_planet_properties)
@@ -9,3 +10,22 @@ case1000 = df_planet_properties[df_planet_properties['Case'] == '1000_F_earth'].
 
 df_atmospheric_profile_case1 = pd.read_csv("atmospheres/ProteusGoogleDrive/H2_atmosphere_1_M_earth_1_F_earth.csv", sep='\t')
 df_atmospheric_profile_case1000 = pd.read_csv("atmospheres/ProteusGoogleDrive/H2_atmosphere_1_M_earth_1000_F_earth.csv", sep='\t')
+
+
+atm_H2_case1 = Atmosphere(
+    #bulk properties
+    M_p=case1['M_planet [kg]'],
+    R_p=case1['R_int [m]'],
+    F_xuv=case1['F_ins [W/m2]'],
+
+    #composition dependent properties
+    T_wind=10**4, 
+    nu_0 = 3.288467085473 * 10**15, 
+    mu_wind=0.5, 
+    mu_plus_wind=1,
+
+    #atmospheric profiles
+    temperatures=df_atmospheric_profile_case1['Temperature [K]'].values,
+    pressures=df_atmospheric_profile_case1['Pressure [Pa]'].values,
+    heights=df_atmospheric_profile_case1['Height [m]'].values
+)
