54b7a366573a681d345ef0bc812f647e65a7c8df
ma1inda <malinleirv@gmail.com>
2026-05-01 12:17:34 +0200

    Altered simple H2 atmosphere to emulate GJ 1214 b

--- atmospheres/simple_H2.py
+++ atmospheres/simple_H2.py
@@ -1,5 +1,6 @@
 import scipy as sp
 import numpy as np
+from conversions import *
 
 G = sp.constants.G #[m^3 kg^-1 s^-2]
 m_p = sp.constants.m_p #[kg]
@@ -12,14 +13,14 @@ T_wind       = 10**4                        #[K]         temperature of the esca
 mu_wind      = 0.5                          #[dimless]   total mean molecular weight of the escaping atmosphere assumed to be entirely ionised
 mu_plus_wind = 1.0                          #[dimless]   mean molecular weight of the ions in the escaping atmosphere assumed to be entirely ionised
 
-#testcase values for the other necessary input parameters for the rr_escape_rate function using jupiter mass and radius, and a typical XUV flux for a close-in exoplanet
-M_p          = 1.898 * 10**27               #[kg]        planetary mass
+#testcase values for GJ 1214 b (sub-neptune) from Salz et. al. 2016
+M_p          = 8.41 * 5.9722 * 10**24       #[kg]        planetary mass
 #R_base     = 7.1492 * 10**7                #[m]         radius of the base of the escaping atmosphere
-F_xuv        = 1.0 * 10**9                  #[kg s^-3]    XUV flux
+F_xuv        = 10**2.93 * erg_to_joule * cm_to_m**(-2) #[kg s^-3]    XUV flux
 nu_0         = 3.288467085473 * 10**15      #[Hz]        frequency of Lyman-alpha radiation, which is the ionising radiation for HI
-R_p          = 7.0 * 10**7                  #[m]         planetary radius
+R_p          = 2.73 * 6.371 * 10**6         #[m]         planetary radius
 P_0          = 2000                         #[Pa]        pressure at the optical photosphere
-T_eq         = 1500                         #[K]         equilibrium temperature of the planet
+T_eq         = 553                         #[K]         equilibrium temperature of the planet
 mu_photo     = 2                            #[dimless]   mean molecular weight at the optical photosphere, assumed to be entirely molecular hydrogen
 
 radii = np.linspace(R_p, 5*R_p, 1000) #[m] array of radii from the planetary radius to 10 times the planetary radius
