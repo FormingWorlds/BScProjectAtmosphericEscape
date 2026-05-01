e77947a87d92c15c145f5640c9ff466bf6781324
ma1inda <malinleirv@gmail.com>
2026-05-01 14:25:16 +0200

    Imported H2 data from proteus on google drive

--- proteus_fetch.py
+++ proteus_fetch.py
@@ -4,7 +4,8 @@ import pandas as pd
 df_planet_properties = pd.read_csv("atmospheres/ProteusGoogleDrive/planet_bulk_properties_H2_atmospheres.csv", sep='\t')
 #print(df_planet_properties)
 
-case_1 = df_planet_properties[df_planet_properties['Case'] == '1_F_earth'].iloc[0]
-case_1000 = df_planet_properties[df_planet_properties['Case'] == '1000_F_earth'].iloc[0]
-print(case_1)
-print(case_1000)
+case1 = df_planet_properties[df_planet_properties['Case'] == '1_F_earth'].iloc[0] #iloc needed to convert table to number. df[df..] mean we grab whole row where the case is true
+case1000 = df_planet_properties[df_planet_properties['Case'] == '1000_F_earth'].iloc[0]
+
+df_atmospheric_profile_case1 = pd.read_csv("atmospheres/ProteusGoogleDrive/H2_atmosphere_1_M_earth_1_F_earth.csv", sep='\t')
+df_atmospheric_profile_case1000 = pd.read_csv("atmospheres/ProteusGoogleDrive/H2_atmosphere_1_M_earth_1000_F_earth.csv", sep='\t')
