e612c5fcf8000f2720a7a105425cca6a35470e66
ma1inda <malinleirv@gmail.com>
2026-05-01 15:41:11 +0200

    Refined readability of diagnostics output

--- road_runner.py
+++ road_runner.py
@@ -12,4 +12,13 @@ from atmospheres.simple_H2 import atm_H2
 atm = atm_H2_case1
 P_base = 10**(-4) #[Pa] pressure at the base of the escaping atmosphere, REFERENCE Lopez et. al. 2017
 
-print(get_rr_escape_diagnostics(atm, P_base))
+results = get_rr_escape_diagnostics(atm, P_base)
+
+print("Escape diagnostics for the atmosphere:")
+print(f"R_base: {results['R_base [m]']:.2g} m")
+print(f"c_s: {results['c_s [m/s]']:.2g} m/s")
+print(f"R_s: {results['R_s [m]']:.2g} m")
+print(f"rho_s: {results['rho_s [kg/m^3]']:.2g} kg/m^3")
+print(f"Escape rate: {results['escape_rate [kg/s]']:.2g} kg/s")
+if results['R_s [m]'] == results['R_base [m]']:
+    print("NB: Escape is not radiation-recombination-limited.")
