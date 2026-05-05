157e9315ce203b60f417a84f0b9563ffc782b95f
ma1inda <malinleirv@gmail.com>
2026-05-05 09:44:28 +0200

    Added T over P plotting function

--- plots/plotter.py
+++ plots/plotter.py
@@ -43,3 +43,23 @@ def plot_T_over_R(atm):
     plt.grid()
     plt.savefig(f"{output_dir}/temperature_over_radius.png", dpi=300)
     plt.close()
+
+def plot_T_over_P(atm):
+    '''
+    Plots the temperature profile of the atmosphere as a function of pressure.
+
+    Takes input parameters: atmosphere object with attributes pressures and temperatures.
+
+    All calculations done in SI units.
+    '''
+    plt.figure(figsize=(8,6))
+    plt.plot(atm.pressures, atm.T)
+    plt.xlabel("Pressure (Pa)")
+    plt.ylabel("Temperature (K)")
+    plt.title("Temperature profile of the atmosphere")
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.grid()
+    plt.savefig(f"{output_dir}/temperature_over_pressure.png", dpi=300)
+    plt.close()
+
