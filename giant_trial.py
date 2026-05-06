15b53a73f8bcb4502982d1963e80af51b637db23
renskewfb <renskebeuker@gmail.com>
2026-05-06 12:16:11 +0200

    Fixed x-axis values and added title

--- giant_trial.py
+++ giant_trial.py
@@ -17,18 +17,19 @@ M = 5.98e24     # mass of planet: Earth (kg)
 
 
 # v_ratio = (v_imp/v_esc)*(m/M)
-v_ratio = np.arange(0.0, 1.0, 100)
+v_ratio = np.linspace(0.0, 1.0, 100)
 v_equ = m/M
 print(v_equ)
 
 X_loss_iso = 0.4*(v_ratio) + 1.4*(v_ratio)**2 - 0.8*(v_ratio)**3
 
 
-plt.plot(v_ratio, X_loss_iso)
+plt.plot(v_ratio, X_loss_iso, color='red')
+plt.axvline(v_equ, color='navy')
 plt.xlabel('(v_imp/v_esc)(m/M)')
 plt.ylabel('X_loss')
+plt.title('X_loss from Mars-like giant impact on isothermal Earth')
 #plt.xscale('log')
 #plt.yscale('log')
 plt.show()
-plt.savefig(isothermal_model_giant)import numpy as np
-
+#plt.savefig(isothermal_model_giant)
