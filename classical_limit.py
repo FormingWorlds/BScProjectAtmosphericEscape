104aae54986a608c3eaa0649d409b111cd3dc161
vstrelcs <viesturs.strelcs123@gmail.com>
2026-05-06 16:03:10 +0200

    fixed unit discrepancy, values now align with Yelle

--- classical_limit.py
+++ classical_limit.py
@@ -12,16 +12,16 @@ K         = 3e6                                        #eddy diffusion coefficie
 alpha     = -0.25                                      #from Yelle 
 M_p       = 6.417e23                                   #mass of Mars, [kg]
 R_p       = 3389.5e3                                   #radius of Mars, [m]
-M_CO2     = 44.0095*10e-3                              #molar mass, [kg/mol]
-M_H       = 1*10e-3                                    #molar mass, [kg/mol]
-mole_frac = 10e-5                                      #mole fraction of H
+M_CO2     = 44.0095*1e-3                              #molar mass, [kg/mol]
+M_H       = 1*1e-3                                    #molar mass, [kg/mol]
+mole_frac = 1e-5                                      #mole fraction of H
 m_a       = (mole_frac*M_H + (1-mole_frac)*M_CO2)/N_A  #mean molecular mass, [kg]
 m_i       = M_H/N_A                                    #minor constituent mean molecular mass, [kg]
 
 #initial grid control. the exobase temperature, T_inf, is left as an input parameter
 z_0     = 80e3   #height above surface at bottom of the model, [m]
 p_0     = 0.1    #pressure defining bottom of the model, [Pa]
-p_inf   = 10e-6  #pressure defining roughly the exobase, [Pa]
+p_inf   = 1e-6  #pressure defining roughly the exobase, [Pa]
 p_steps = 10000   #atmosphere layers
 T_0     = 100    #temperature at the bottom of the model (p_0), [K]
 
@@ -50,7 +50,7 @@ def classical_limiting_flux(T_inf):
         z[i+1] = z[i] + dz
     
     #3. number density profile, simply from ideal gass
-    n = (p)/(k*T) * 10e-6  #[1/cm^3]
+    n = (p)/(k*T) * 1e-6  #[1/cm^3]
     
     #4. homopause location
     b = A*(T**s)  #binary diffusion parameter, [1/cm*s]
@@ -59,35 +59,44 @@ def classical_limiting_flux(T_inf):
     D       = b/n                #[cm^2/s]
     diffs   = np.abs(K-D)
     hom_arg = np.argmin(diffs)
-    b_hom   = b[hom_arg] * 10e2  #[1/m*s]
+    b_hom   = b[hom_arg] * 1e2  #[1/m*s]
     
     #5. limiting flux calculation
     #start with scale height 
-    H = (k*T[0]*((z[0]+R_p)**2))/(G*M_p*m_a)
+    H = (k*T[hom_arg]*((z[hom_arg]+R_p)**2))/(G*M_p*m_a)
     
     #then we need dT/d(xi) at the homopause, which via algebra is -dT/dp p
     T_grad  = - ((T[hom_arg+1]-T[hom_arg-1])/((p[hom_arg+1]-p[hom_arg-1]))) * p[hom_arg]
     m_tilde = m_i + (alpha*T_grad*(m_a/T[hom_arg]))
     
-    flux_per_area = mole_frac * (b_hom/H) * (1-(m_tilde/m_a)) * 10e-4  #[1/cm^2*s]
-    flux_tot      = flux_per_area * 4*np.pi*((z[hom_arg]+R_p)**2)      #[1/s]
+    flux_per_area = mole_frac * (b_hom/H) * (1-(m_tilde/m_a)) * 1e-4  #[1/cm^2*s]
+    flux_tot      = flux_per_area * 4*np.pi*(((z[hom_arg]+R_p)*100)**2)      #[1/s]  !!!
     
-    return(flux_per_area, flux_tot)
+    return(flux_per_area, flux_tot, n[-1])
 
 
 #now we perform an exobase temperature sweep and plot the results
 T_sweep = np.arange(100, 400)
 flux_list = []
+exobase_n_list = []
 
 for temp in T_sweep:
     results = classical_limiting_flux(temp)
     flux_list.append(results[0])
+    exobase_n_list.append(results[2])
     
 fluxes = np.asarray(flux_list, dtype='float64')
+exobase_n = np.asarray(exobase_n_list, dtype='float64')
 
 plt.plot(T_sweep, fluxes)
 plt.xlabel('Exobase temperature [K]')
 plt.ylabel('Escape flux [cm$^{-2}$ $\cdot$ s$^{-1}$]')
+plt.grid()
+plt.show()
+
+plt.plot(T_sweep, exobase_n)
+plt.xlabel('Exobase temperature [K]')
+plt.ylabel('Exobase number density [cm$^{-3}$]')
 plt.grid()
 plt.show()
 
