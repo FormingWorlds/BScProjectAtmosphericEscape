4e1c89e7e7af4f2133ea7b719a47cb58ee9bb494
renskewfb <renskebeuker@gmail.com>
2026-05-06 12:54:32 +0200

    Add Earth values and mass formulas

--- planetesimal_trial.py
+++ planetesimal_trial.py
@@ -1,3 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+r_min = 1	# minimum impact (km)
+rho_0 = 1.293	# density at earth (kg/m^3)
+h =  8.5	# scale height earth atmosphere (km)
+R = 12756	# Radius earth (km)
+
+
+m_small = 4*np.pi*rho_0*(h**3)
+print(m_small)
+
+m_large = rho_0*(np.pi*h*R)**(3/2)*2**(0.5)
+
+#if m > m_small:
+
+
