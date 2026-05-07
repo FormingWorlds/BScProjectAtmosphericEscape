e5e99812a8f71d466face8b1b41e6d1230f78360
ma1inda <malinleirv@gmail.com>
2026-05-07 14:44:20 +0200

    Adjusted atm examination program for the new proteus data method

--- road_runner.py
+++ road_runner.py
@@ -2,13 +2,14 @@
 from rr_escape import *
 
 ### Importing the single atmospheres we want to examine ###
-from proteus_fetch import atm_H2_case1
-#from proteus_fetch import atm_H2_case1000
+from proteus_fetch import proteus_atmosphere_cases
+
 #from atmospheres.simple_H2 import atm_H2
 #from atmospheres.simple_H2He_mix import atm_H2He_mix
 #from atmospheres.simple_H2O import atm_H2O
 
 
 ### Doing escape calculations ###
-atm = atm_H2_case1
+#print(dict.keys(proteus_atmosphere_cases))
+atm = proteus_atmosphere_cases['H2O_1_M_earth_1000_F_earth']
 examine_atmosphere_for_rr_escape(atm)
