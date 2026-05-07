e05efb2eb2a788ebb1fb91af8abb0b454349d8d6
ma1inda <malinleirv@gmail.com>
2026-05-07 10:33:59 +0200

    Commented out unnecessary imports

--- road_runner.py
+++ road_runner.py
@@ -1,14 +1,14 @@
 ### Importing the escape functions ###
 from rr_escape import *
 
-### Importing the atmospheres we want to examine ###
+### Importing the single atmospheres we want to examine ###
 from proteus_fetch import atm_H2_case1
-from proteus_fetch import atm_H2_case1000
-from atmospheres.simple_H2 import atm_H2
-from atmospheres.simple_H2He_mix import atm_H2He_mix
-from atmospheres.simple_H2O import atm_H2O
+#from proteus_fetch import atm_H2_case1000
+#from atmospheres.simple_H2 import atm_H2
+#from atmospheres.simple_H2He_mix import atm_H2He_mix
+#from atmospheres.simple_H2O import atm_H2O
 
 
 ### Doing escape calculations ###
-atm = atm_H2
+atm = atm_H2_case1
 examine_atmosphere_for_rr_escape(atm)
