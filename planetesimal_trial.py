a356b05743753e1f4954fe5702675e83f9249855
renskewfb <renskebeuker@gmail.com>
2026-05-13 13:29:22 +0200

    Continue with code fig 14

--- planetesimal_trial.py
+++ planetesimal_trial.py
@@ -5,7 +5,13 @@ r_min = 1	# minimum impact (km)
 rho_0 = 1.293	# density at earth (kg/m^3)
 h =  8.5	# scale height earth atmosphere (km)
 R = 12756	# Radius earth (km)
+M_Earth = 5.97e24	# mass earth (kg)
+M_atm = 5.15
 
+r = np.linspace(1,1000, 1000)
+
+r_cap = 25
+r_min
 
 m_small = 4*np.pi*rho_0*(h**3)
 print(m_small)
