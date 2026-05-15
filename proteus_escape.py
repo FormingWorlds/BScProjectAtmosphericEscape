3dabff602057d24861eb145b3e37e1798e008c5d
ioana07x <balintioanaroxana@gmail.com>
2026-05-15 19:43:04 +0200

    Added check to see if profiles are consistent

--- proteus_escape.py
+++ proteus_escape.py
@@ -1,34 +1,10 @@
 import os
-import glob
 import numpy as np
 import pandas as pd
 from scipy.constants import k, atomic_mass
 
 from jeans import jeans_escape
-
-
-R_EARTH = 6.371e6
-M_EARTH = 5.972e24
-
-SPECIES_MASSES = {
-    "H": 1 * atomic_mass,
-    "H2": 2 * atomic_mass,
-    "He": 4 * atomic_mass,
-    "C": 12 * atomic_mass,
-    "N": 14 * atomic_mass,
-    "O": 16 * atomic_mass,
-    "N2": 28 * atomic_mass,
-    "O2": 32 * atomic_mass,
-    "CO": 28 * atomic_mass,
-    "CO2": 44 * atomic_mass,
-    "H2O": 18 * atomic_mass,
-    "OH": 17 * atomic_mass,
-    "NO": 30 * atomic_mass,
-    "NO2": 46 * atomic_mass,
-    "N2O": 44 * atomic_mass,
-    "CH4": 16 * atomic_mass,
-    "NH3": 17 * atomic_mass,
-}
+from constants import *
 
 def read_proteus_profile(path, R_planet, min_vmr=1e-21):
     df = pd.read_csv(path, sep="\t")
@@ -54,8 +30,21 @@ def read_proteus_profile(path, R_planet, min_vmr=1e-21):
             continue
 
         species[sp] = vmr * n_tot
+    print("\nFILE:", path)
+    print("first row:")
+    print("height =", df["Height [m]"].iloc[0])
+    print("P      =", df["Pressure [Pa]"].iloc[0])
+    print("T      =", df["Temperature [K]"].iloc[0])
 
+    print("last row:")
+    print("height =", df["Height [m]"].iloc[-1])
+    print("P      =", df["Pressure [Pa]"].iloc[-1])
+    print("T      =", df["Temperature [K]"].iloc[-1])
+
+    print("height increasing?", np.all(np.diff(df["Height [m]"].to_numpy()) > 0))
+    print("pressure decreasing?", np.all(np.diff(df["Pressure [Pa]"].to_numpy()) < 0))
     return r, T, species, df
+
 
 def read_bulk_properties(path, case):
     bulk = pd.read_csv(path, sep="\t")
@@ -81,7 +70,7 @@ def run_one_file(path, bulk, sigma):
         species=species,
         species_masses=SPECIES_MASSES,
         M=M_planet,
-        sigma=sigma,
+        sigma="weighted",
         dayside=True,
     )
 
