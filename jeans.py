d5cee7e67f69d119b967a7cb9818d7e3181b7149
ioana07x <balintioanaroxana@gmail.com>
2026-05-15 19:33:34 +0200

    Modified jeans with the cross section

--- jeans.py
+++ jeans.py
@@ -1,13 +1,21 @@
 from physics import *
 from exobase import *
+from cross_section import *
 
 # escape rate
 def jeans_escape(r, T, species, species_masses, M, sigma = 1e-19, dayside=True):
     
     n_tot = sum(species.values())
     m_mean = mean_mass(species, species_masses)
+
+    if sigma == "weighted":
+        sigma_used = effective_cross_section(species)
+    else:
+        sigma_used = sigma
     
-    idx = find_exobase(r, n_tot, T, m_mean, M, sigma)
+    idx = find_exobase(r, n_tot, T, m_mean, M, sigma_used)
+
+    sigma_exo = sigma_used[idx] if np.ndim(sigma_used) > 0 else sigma_used
     
     r_exo, T_exo = r[idx], T[idx]
     A = (2 if dayside else 4) * pi * r_exo**2
@@ -44,5 +52,6 @@ def jeans_escape(r, T, species, species_masses, M, sigma = 1e-19, dayside=True):
         "exobase_index": int(idx) , 
         "exobase_radius": float(r_exo),
         "temperature": float(T_exo), 
-        "exobase_altitude": float(exobase_altitude)
+        "exobase_altitude": float(exobase_altitude),
+        "sigma_exobase_m2": float(sigma_exo),
     }
