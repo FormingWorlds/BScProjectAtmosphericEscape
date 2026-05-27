4e066ec3f5d783a999ead0f022d16dab03aa9fa3
ma1inda <malinleirv@gmail.com>
2026-05-27 11:26:09 +0200

    Created function for calculating case A. Imported manually the case A and ground state rr_coeffs for He, C, O, N. Calculated case B for them.

--- radiation_recombination_coefficient_cauldron.py
+++ radiation_recombination_coefficient_cauldron.py
@@ -1,7 +1,10 @@
 ### This file is for calculating the radiation recombination coefficient for the different species ###
 
+### Imports ###
 import numpy as np
+import pandas as pd
 
+### Functions ###
 def calculate_rr_coefficient_case_A(T, T_0, T_1, A, B, C=None, T_2=None):
     '''
     Calculates the radiation recombination coefficient for case A for your species and environment of interest.
@@ -12,6 +15,8 @@ def calculate_rr_coefficient_case_A(T, T_0, T_1, A, B, C=None, T_2=None):
     Outputs: radiation recombination coefficient [cm^3 s^-1]
 
     Designed to take input parameters from data from AMDPP (Atomic and Molecular Diagnostics Processes in Plasma).
+    Formula for calculating the radiation recombination coefficient is from AMDPP Fits for Total Radiative Recombination Coefficients, link: https://amdpp.phys.strath.ac.uk/tamoc/DATA/RR/RR_web/adf48/
+ 
     '''
 
     # Checks if the optional parameters C and T_2 are provided, meaning we are working with a low charge atom, and if so, calculates the effective B parameter (B_eff) using the provided formula. If not, it sets B_eff to be equal to B.
@@ -25,3 +30,44 @@ def calculate_rr_coefficient_case_A(T, T_0, T_1, A, B, C=None, T_2=None):
     alpha_rr_case_A = A * ( np.sqrt(T / T_0) * (1 + np.sqrt(T / T_0))**(1-B_eff) * (1 + np.sqrt(T / T_1))**(1+B_eff) )**(-1)
 
     return alpha_rr_case_A
+
+
+### Fetch Data from AMDPP archive ###
+
+# Directory from which to retrieve that data
+input_dir = 'coefficients_calculation_data/'
+
+# Want to loop over these names to get the files
+e_Z = np.array(['b_c', 'c_n', 'h_he', 'n_o']) #e means as many electrons as the charge of that ion, while Z indicated the charge of the nucles of the atom. For example b_c means carbon with 5 electrons, so C+ (carbon ionised once), c_n means nitrogen with 6 electrons and so forth.
+
+#for name in e_Z:
+    #filepath = f'{input_dir}nrb05#{name}1ls.dat'
+
+### Manually fetching the data for one species to test the function, and then I can loop over the species to get the coefficients for all of them ###
+CI_GS_coeff = 2.32 * 10**(-13) #cm^3 s^-1 #From nrb05#b_c1ls.dat, data under PRTI= 1  TRMPRT= (2P)  SPNPRT= 2, coeff under TE index 1.00E+04
+
+CI = np.array([]) #array to store the coefficients for carbon ionised once (C+)
+
+############################################################################
+
+### Fetched the data manually for the species currently of interest ###
+rr_coefficients_case_A = {
+    # Assumes all of them to be singly ionised and T=10 000 K, and the files are all using LS coupling (no fine structure levels). Took values from the ARR(FIT) column. REFERENCE: cfout_K file from AMDPP archive, link: https://amdpp.phys.strath.ac.uk/tamoc/DATA/RR/RR_web/adf48/clist_K
+    'C': 4.72 * 10**(-13), #[cm^3 s^-1]  from row Z= 6 N= 5 M= 1 W= 2        Z means nuclear charge, N means number of electrons, M means number of metastable levels, W means statistical weight of the inital state,
+    'He': 4.37 * 10**(-13), #[cm^3 s^-1] from row Z= 2 N= 1 
+    'O': 2.72 * 10**(-13), #[cm^3 s^-1]  from row Z= 8 N= 7 M= 1 W= 4
+    'N': 3.76 * 10**(-13), #[cm^3 s^-1]  from row Z= 7 N= 6 M= 1 W= 1
+}
+
+rr_coefficients_ground_state = {
+    'C':2.32 * 10**(-13), #[cm^3 s^-1] from nrb05#b_c1ls.dat, data under  PRTI= 1  TRMPRT= (2P)  SPNPRT= 2, coeff under TE index 1.00E+04
+    'He': 1.56 * 10**(-13), #[cm^3 s^-1] from nrb05#h_he1ls.dat, data under PRTI= 1  TRMPRT= (2S)  SPNPRT= 2, coeff under TE index 1.00E+04
+    'O': 1.31 * 10**(-13), #[cm^3 s^-1] from nrb05#n_o1ls.dat, data under  PRTI= 1  TRMPRT= (4S)  SPNPRT= 4, coeff under TE index 1.00E+04
+    'N': 1.15 * 10**(-13), #[cm^3 s^-1] from nrb05#c_n1ls.dat, data under   PRTI= 1  TRMPRT= (3P)  SPNPRT= 3, coeff under TE index 1.00E+04
+}
+
+
+### Calculating the coefficients for the ionised species ###
+rr_coefficients_case_B = {}
+for species in rr_coefficients_case_A.keys():
+    rr_coefficients_case_B[species] = rr_coefficients_case_A[species] - rr_coefficients_ground_state[species] #[cm^3 s^-1] case B coefficient is case A coefficient minus the coefficient for recombinations to the ground state, where case A coefficient: radiation recombination coefficient for case A, coefficient for recombinations to the ground state: radiation recombination coefficient for recombinations to the ground state
