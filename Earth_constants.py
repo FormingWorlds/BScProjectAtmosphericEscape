1b4ef2608fabdca2f393fd29885c457973dcfba3
renskewfb <renskebeuker@gmail.com>
2026-06-03 10:18:40 +0200

    Python file with Earth constants

---
+++ Earth_constants.py
@@ -0,0 +1,8 @@
+R = 6378e3		# Earth radius [m]
+M = 5.98e24		# Earth mass [kg]
+h = 8.5e3		# scaleheight [m]
+M_atm = 5.15e18		# Earth atmosphere mass [kg]
+rho_atm = 1.293		# density Earth atmosphere at 0 height [kg/m^3]
+rho_pl = 2000		# density planetesimal [kg/m^3]
+
+
