decdd63e37af56f4de83830ac46b819960d14bcd
vstrelcs <viesturs.strelcs123@gmail.com>
2026-06-05 17:15:35 +0200

    .csv of only H and full disso cases added

--- proteus_output.py
+++ proteus_output.py
@@ -75,4 +75,4 @@ export_dict = {
 }
                 
 df = pd.DataFrame(export_dict)
-df.to_csv('proteus_diffusion_limit_results.csv', index=False)
+df.to_csv('proteus_diffusion_limit_results_full_disso.csv', index=False)
