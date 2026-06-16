f5d0b4794657e382d4eb842ebdfe5047312e4a8c
ma1inda <malinleirv@gmail.com>
2026-06-16 10:44:46 +0200

    Created skeleton for EL escape function.

---
+++ el_escape.py
@@ -0,0 +1,20 @@
+### This file will give the energy limited rate of escape for an atmosphere ###
+
+import numpy as np
+from rr_escape import calc_sound_speed, calc_sonic_point_radius
+
+def el_escape_rate(epsilon_xuv, F_xuv, R_base, M_p):
+    '''
+    This function returns the energy limited escape rate for an atmosphere following the formulation in Lopez 2017 (eq 2).
+
+
+    Inputs:
+    epsilon_xuv (float): species dependent evaporation efficiency [unitless]
+    F_xuv (float): XUV flux [W/m^2]
+    R_base (float): base radius of the atmosphere [m]
+    M_p (float): planetary mass [kg]
+
+    Output: 
+    el_escape_rate (float): energy limited escape rate [kg/s]
+    '''
+    
