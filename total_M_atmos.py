ee369a83591d3f2adc213f96bbca3ab98938e39a
renskewfb <renskebeuker@gmail.com>
2026-07-27 12:03:22 +0200

    Import numpy within function and removed comment

--- total_M_atmos.py
+++ total_M_atmos.py
@@ -1,7 +1,7 @@
-# Total atmosphere mass is calculated by integrating 4*pi*rho*(r+R)**2
-
 def total_M_atmos(height, rho_atm, R):
-	"""Calculate the total mass of the atmosphere using the height and density profiles, as well as the radius of the planet"""
+	"""Calculate the total mass of the atmosphere using the height and density profiles, as well as the radius of the planet"""	
+	import numpy as np
+
 	int_values = []
 	for r, d in zip(height, rho_atm):
 		int_val = d * (R+r)**2
