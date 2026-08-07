a595c2e26f47af19a8060340f0ccd19a3a0b2a6a
renskewfb <renskebeuker@gmail.com>
2026-08-07 11:42:23 +0200

    Added Boltzmann constant and Avogadro's number

--- Earth_constants.py
+++ Earth_constants.py
@@ -1,8 +1,12 @@
-R = 6378e3		# Earth radius [m]
-M = 5.98e24		# Earth mass [kg]
-h = 8.5e3		# scaleheight [m]
-M_atm = 5.15e18		# Earth atmosphere mass [kg]
-rho_atm = 1.293		# density Earth atmosphere at 0 height [kg/m^3]
+from scipy.constants import k, N_A
+
+R_Earth = 6378e3	# Earth radius [m]
+M_Earth = 5.98e24	# Earth mass [kg]
+h_Earth = 8.5e3		# scaleheight [m]
+M_atm_Earth = 5.15e18	# Earth atmosphere mass [kg]
+rho_atm_Earth = 1.293	# density Earth atmosphere at 0 height [kg/m^3]
 rho_pl = 2000		# density planetesimal [kg/m^3]
 
+N_A = N_A		# Avogadro's number
+k = k			# Boltzmann constant
 
