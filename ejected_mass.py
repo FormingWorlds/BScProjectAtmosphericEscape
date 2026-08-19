b7e6766e0f337a9f1702c4138674f67c3d234411
renskewfb <renskebeuker@gmail.com>
2026-08-19 06:41:18 +0200

    Fixed equation for ejected mass by planetesimal impactors, and added more functions in one .py file

--- ejected_mass.py
+++ ejected_mass.py
@@ -43,23 +43,63 @@ def cap_mass(rho_atm, h, R):
 
 
 
-def ejected_mass_planetesimal(r, M_atm, h, R, rho, rho_pl):
-    """Ejected mass by a planetesimal impactor (r < r_gi)
-       Once r > r_cap, the cap mass is 
-    """
-    r_min = r_min(rho, rho_pl, h)
-    r_cap = r_cap(rho, rho_pl, h, R)
-    
-    if r < r_min:
-        print("No mass loss possible due to escape.")
-        
-    elif r >= r_min and r < r_cap:
-        M_eject = (r_min/(2*r)) * (1 - (r_min/r)**2)
-    
-    if r >= r_cap:
-        M_eject = cap_mass(rho, h, R)
-        
-    return M_eject
+def ejected_mass_planetesimal(M_atm, h, R, rho, rho_pl):
+	"""Ejected mass by a planetesimal impactor (r < r_gi)
+	Once r > r_cap, the cap mass is
+	"""
+	r_min_val = r_min(rho, rho_pl, h)
+	r_cap_val = r_cap(rho, rho_pl, h, R)
+	r_gi_val = r_gi(h, R)
+
+	r_range = np.linspace(r_min_val, r_gi_val, 1000)
+	M_ejected = []
+
+	for r in r_range:
+
+		if r < r_min_val:
+			M_eject = 0.0
+			print("No mass loss possible due to too small impactor size.")
+
+		elif r >= r_min_val and r < r_cap_val:
+			m_imp = rho_pl * (4/3) * np.pi * r**3
+			M_eject = (r_min_val/(2*r)) * (1 - (r_min_val/r)**2) * m_imp
+
+		elif r >= r_cap_val:
+			M_eject = cap_mass(rho, h, R)
+
+		M_ejected.append(M_eject)
+
+	M_ejected = np.array(M_ejected)
+
+	return M_ejected, r_range
+
+
+# def ejected_mass_planetesimal(M_atm, h, R, rho, rho_pl):
+# 	"""Ejected mass by a planetesimal impactor (r < r_gi)
+# 	Once r > r_cap, the cap mass is
+# 	"""
+# 	r_min_val = r_min(rho, rho_pl, h)
+# 	r_cap_val = r_cap(rho, rho_pl, h, R)
+# 	r_gi_val = r_gi(h, R)
+
+# 	r = np.linspace(r_min_val, r_gi_val, 1000)
+
+# 	no_regime = (r < r_min_val)
+# 	regime1 = (r >= r_min_val) and (r < r_cap_val)
+# 	regime2 = (r >= r_cap_val) and (r < r_gi)
+# 	regime3 = (r >= r_gi)
+
+# 	if no_regime.any():
+# 		print("Radius of impactor too small to eject atmosphere")
+
+# 	M_eject[regime1] = (r_min_val/(2*r)) * (1 - (r_min_val/r)**2)
+# 	M_eject[regime2] = cap_mass(rho, h, R)
+
+# 	if regime3.any():
+# 		print("Impactor big enough to eject all mass: global loss")
+
+
+# 	return M_eject
 
 
 
