af8c442e74cf4f45cd4b78cbd870c3cd4192ed99
renskewfb <renskebeuker@gmail.com>
2026-08-21 10:18:43 +0200

    Added input for v_imp/v_esc

--- giant_impacts_fractional_loss.py
+++ giant_impacts_fractional_loss.py
@@ -3,15 +3,17 @@ import numpy as np
 # x = (v_imp*m) / (v_esc*M) --> x-axis of graphs
 
 
-def X_loss(gamma):
+def X_loss(gamma, v_V):
         """Fractional mass loss of atmosphere
         x = (v_imp*m) / (v_esc*M)       --> x-axis
-        Gamma: adiabatic index
+        v_V = (v_imp)/(v_esc)
+	Gamma: adiabatic index
                 - 5/3 (isothermal)
                 - 4/3 (adiabatic)
         """
-        x = np.linspace(0.0, 1.0, 100)
-
+        m_M = np.linspace(0.0, 1.0, 100)
+	x = v_V * m_M
+	
         if gamma == 5/3:
                 X_loss = 0.4*x + 1.4*(x**2) - 0.8*(x**3)
 
